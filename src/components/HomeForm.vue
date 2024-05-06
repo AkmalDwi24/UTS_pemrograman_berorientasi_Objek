09a38c2a1d078ed05eb6368ff5c613d48dedb758
M.Akmal <ma808509@hmail.com>
2024-05-06 12:19:56 +0700

    first commit

---
+++ src/components/HomeForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="outer-box">
+    <h2>Selamat Datang Di aplikasi Sistem Penjadwalan Kuliah</h2>
+    <div class="home">
+      <p>Aplikasi Penjadwalan Kuliah ini Dibuat oleh:</p>
+      <ul>
+        <li>Nama : Muhammad Akmal Dwi mulyono</li>
+        <li>NPM : 40622100031</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeFrom'
+}
+</script>
+
+<style scoped>
+h2 {
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 10px;
+}
+
+.outer-box {
+  border: 1px solid #ccc;
+  padding: 20px;
+  width: 300px;
+  height: 200px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin-top: 100px;
+  margin-left: 270px;
+  width: 500px;
+  height: 230px;
+}
+
+.home {
+  margin-top: 20px;
+}
+</style>
---
+++ src/components/navbar/LeftNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-navigation-drawer expand-on-hover rail>
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-img src="d:\Ne\unnamed.png"></v-img>
+      <v-list-item prepend-icon="mdi-folder" title="Home" :to="'/'"></v-list-item>
+      <v-list-item prepend-icon="mdi-account-multiple" title="Dosen" :to="'/Dosen'"></v-list-item>
+      <v-list-item prepend-icon="mdi-star" title="Mahasiswa" :to="'/Mahasiswa'"></v-list-item>
+      <v-list-item prepend-icon="mdi-star" title="Ruangan" :to="'/Ruangan'"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
